aa4f9b6758164359659d2c76d0e39cc97493cb22
Ygor Dev <ygorneves190@gmail.com>
2023-05-22 15:46:29 -0300

    Teste

---
+++ src/teste01.java
@@ -0,0 +1,16 @@
+public class teste01 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        char b = 'a';
+        long c = 400;
+        double d = 3.2;
+
+        System.out.println("Valor de a = " + a);
+        System.out.println("Valor de b = " + b);
+        System.out.println("Valor de c = " + c);
+        System.out.println("Valor de d = " + d);
+
+    
+    }
+}
